aaf5d94e17c0fbd24027451827240e8923b22fd4
Bradley C <bradleyctclarke@gmail.com>
2019-02-16 01:50:35 -0700

    adding helpers & core

---
+++ contracts/TidbitsCore.sol
@@ -0,0 +1,11 @@
+pragma solidity ^0.5.0;
+
+import "openzeppelin-solidity/contracts/token/ERC721/ERC721Enumerable.sol";
+import "./TidbitsHelpers.sol";
+
+contract TidbitsCore is ERC721Enumerable, TidbitsHelpers {
+
+    constructor () public {
+        // this constructor is still under construction
+    }
+}
