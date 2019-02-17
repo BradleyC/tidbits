1f5306f0959fbb1cafeeba1b7079df7a495d1da1
Bradley C <bradleyctclarke@gmail.com>
2019-02-17 05:33:16 -0700

    got deployments working

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,15 +1,16 @@
 const GasAndAccountManager = artifacts.require('./contracts/GasAndAccountManager.sol')
 const TidbitsCore = artifacts.require('./contracts/TidbitsCore.sol')
 const TidbitsHelpers = artifacts.require('./contracts/TidbitsHelpers.sol')
+const ERC20 = artifacts.require('./contracts/ERC20.sol')
 
 module.exports = async deployer => {
   await deployer.deploy(TidbitsHelpers)
   // console.log('Contract address:', c.address)
-
-  deployer.link(TidbitsHelpers, [GasAndAccountManager])
-  await deployer.deploy(GasAndAccountManager, 30, 5000)
   
-  deployer.link(GasAndAccountManager, [TidbitsCore])
+  await deployer.deploy(ERC20)
+  await deployer.deploy(TidbitsCore, 30, 5000)
+  // await deployer.deploy(GasAndAccountManager, 30, 5000)
+  
+  // deployer.link(GasAndAccountManager, [TidbitsCore])
 
-  await deployer.deploy(TidbitsCore)
 }
