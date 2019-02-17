888621393728f5e4d51ff5193b1596570ce587b4
Bradley C <bradleyctclarke@gmail.com>
2019-02-17 00:47:13 -0700

    tidbits core first draft done

--- contracts/TidbitsCore.sol
+++ contracts/TidbitsCore.sol
@@ -1,18 +1,51 @@
 pragma solidity ^0.5.0;
 
-import "./ERC998.sol";
-import "./Accounts.sol";
+import "./GasAndAccountManager.sol";
+import "openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
 
-contract TidbitsCore is ComposableTopDown, Accounts {
-    /* @dev INHERITANCE PATTERN
-        This contract interacts heavily with an external gas and account manager contract.
-        
-     */
-    
-    // wrapper for state changing functions - call them and then register with GasMan
-    // create token
-    // append to parent
+contract TidbitsCore is GasAndAccountManager, TidbitsHelper, ERC20 {
+    string public name = "Tidbit";
+    string public symbol = "TDBT";
+    // who even wants to count pennies anyway!?!
+    uint8 public decimals = 0;
+    uint public INITIAL_SUPPLY = 666666666666;
 
-    constructor (address gam) public {
+    struct Lyric {
+        uint256 parentLyric;
+        // using line number of words from BIP-39 set as index, concat into integer
+        uint128 lyrics;
+        address lyricOwner;
+    }
+
+    Lyric[] public lyrics;
+    uint256 public totalLyrics;
+    mapping (address => uint256[]) addressToLyrics;
+    mapping (uint256 => address) lyricToAddress;
+
+    // create
+    function createLyric(uint256 _parentLyric, uint128 _lyrics) public payable {
+        uint256 startGas = gasleft();
+        address payable sender = msg.sender;
+        Lyric memory lyric = Lyric({
+            parentLyric: _parentLyric,
+            lyrics: _lyrics,
+            lyricOwner: sender
+        });
+        totalLyrics = lyrics.push(lyric) - 1;
+        addressToLyrics[sender].push(totalLyrics);
+        lyricsToAddress[totalLyrics] = sender;
+        emit newLyric(totalLyrics, _parentLyric, _lyrics, sender);
+        transfer(address(this), 5);
+        registerActionAndPassGas(1, startGas, 0, sender); 
+    }
+
+    event newLyric(uint256 lyricId, uint256 parentLyric, uint128 lyrics, address lyricOwner);
+
+    function getLyricsByAddress(address _address) pure returns (uint256[]) {
+        return addressToLyrics[_address];
+    }
+
+    constructor () public {
+        _mint(msg.sender, INITIALSUPPLY);
     }
 }
